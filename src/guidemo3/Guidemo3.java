27209654d652ee9d6f0b139fadc16714f545d660
joel <joelblesswinraj21@gmail.com>
2024-02-14 22:55:41 +0530

    blh

---
+++ src/guidemo3/Guidemo3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guidemo3;
+
+/**
+ *
+ * @author joelb
+ */
+public class Guidemo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new tarea();
+    }
+    
+}
